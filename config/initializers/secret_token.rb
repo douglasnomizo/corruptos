5872be9f0e80ed9b41f7c3cd4b1d0b9914a9e263
Douglas Haruo Lopes Nomizo <douglasnomizo@outlook.com>
2012-10-30 10:06:56 -0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corruptos::Application.config.secret_token = '155430f5a0db8fa3e8c2349eeb4db118bd15b77e3c1d27bdd4c280fe91bb9fd2b412726a948429e17cbdf58589f678990ed6e993e86770d419403071b7ecc5a3'
