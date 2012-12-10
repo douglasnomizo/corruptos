b161f46c42e25d2d38d33eec9d0a0fcc1e9d27cf
Douglas Haruo Lopes Nomizo <douglasnomizo@outlook.com>
2012-12-10 17:18:23 -0200

    Adição de validação para impedir criação de coligações com partidos já coligados

--- app/models/coligacao.rb
+++ app/models/coligacao.rb
@@ -9,11 +9,27 @@ class Coligacao < ActiveRecord::Base
   validate do |coligacao|
     coligacao.errors[:base] << "Cargo indisponível para essa eleição!" if coligacao.cargo_eleicao.blank?
   end
+
   validates :nome, presence: true, length: {in: 5..55}
   validates_uniqueness_of :nome
   validate :valida_qtd_partidos
+  validate :valida_partidos_nao_coligados
 
   def valida_qtd_partidos
     errors.add(:base, "Selecione 2 ou mais partidos para formar uma coligação!") if partidos.size < 2
+  end
+
+  def valida_partidos_nao_coligados
+    p_ids = Coligacao.where(cargo_eleicao_id: self.cargo_eleicao_id).map {|e| e.partido_ids }
+    p_ids.flatten!.uniq!
+    partidos_invalidos = self.partido_ids - p_ids
+    if partidos_invalidos.size > 0 
+      msg = ""
+      partidos_invalidos.each do |pi|
+        msg += Partido.find(pi).nome + ", "
+      end
+      msg += "já coligado(s) para o cargo selecionado!"
+      errors.add(:base, msg)
+    end
   end
 end
